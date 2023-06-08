80ce391883fb5469fbc7cf40a1dc3321dc2b87bf
gogulaganeshgupta <120078187+gogulaganeshgupta@users.noreply.github.com>
2023-06-08 21:53:27 +0530

    import java.util.Scanner; public class solution {     public static void main(String args[])     {         Scanner scan=new Scanner(System.in);         int a=scan.nextInt();         int[] arr = new int[a];         for(int i=0;i<a;i++)         {             arr[i]=scan.nextInt();         }         int d=0;         scan.close();         for(int i=1;i<a;i++)         {             if(arr[i-1]<arr[i])             {                 d++;             }         }         if(d==a-1)         {             System.out.print("yes");         }         else         {             System.out.print("no");         }     } }

---
+++ Ascending_array.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class solution {
+    public static void main(String args[])
+    {
+        Scanner scan=new Scanner(System.in);
+        int a=scan.nextInt();
+        int[] arr = new int[a];
+        for(int i=0;i<a;i++)
+        {
+            arr[i]=scan.nextInt();
+        }
+        int d=0;
+        scan.close();
+        for(int i=1;i<a;i++)
+        {
+            if(arr[i-1]<arr[i])
+            {
+                d++;
+            }
+        }
+        if(d==a-1)
+        {
+            System.out.print("yes");
+        }
+        else
+        {
+            System.out.print("no");
+        }
+    }
+}
