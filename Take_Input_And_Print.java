967eb370d9e9f7b7c9eb4c105854bcdbaa17998c
gogulaganeshgupta <120078187+gogulaganeshgupta@users.noreply.github.com>
2023-06-08 21:37:15 +0530

    import java.util.*; public class KCPD {     public static void main(String arg[])     {         Scanner scan = new Scanner(System.in);         String a = scan.nextLine();         System.out.print(a);         scan.close();     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class KCPD
+{
+    public static void main(String arg[])
+    {
+        Scanner scan = new Scanner(System.in);
+        String a = scan.nextLine();
+        System.out.print(a);
+        scan.close();
+    }
+}
